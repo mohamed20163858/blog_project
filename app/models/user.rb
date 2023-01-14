c31848688d68bdb44483eb3ab4510b880cc84652
Mohamed Saleh <mohamed20163858@gmail.com>
2023-01-14 22:48:59 +0200

    Change postgres password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :bio, presence: true, length: { minimum: 3, maximum: 150 }
-  validates :posts_counter, presence: true, comparison: { greater_than_or_equal_to: 0 },
-                            numericality: { only_integer: true }
+  # validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  # validates :bio, presence: true, length: { minimum: 3, maximum: 150 }
+  # validates :posts_counter, presence: true, comparison: { greater_than_or_equal_to: 0 },
+  #                           numericality: { only_integer: true }
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
--- spec/system/user_index_spec.rb
+++ spec/system/user_index_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
-
 RSpec.describe 'User index page Capybara integration test', type: :system do
+  before do
+    driven_by(:selenium_chrome_headless)
+  end
   it 'test seeing the username of all other users' do
-    visit '/users'
-    expect(page).to have_content('User name: Tom')
+    visit users_path
+    sleep(5)
+    expect(page).to have_text('User name: Tom')
   end
   
 end
